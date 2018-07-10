dc2b1121741775357c7fbc6e049f19a330aa545f
Justin Olson <j.olson.digital@gmail.com>
2018-07-09 22:06:23 -0700

    Added animated gradient to main class, adjusted box box-shadow, cursor, and changed blue to pinkish. Also added work line

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const justin = {
     location: 'Arizona',
     interests: ['Coding', 'Gaming', 'Hiking'],
     languages: ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Bootstrap'],
-    social: ['Facebook', 'LinkedIn', 'Github', 'Instagram'],
+    work: ['Projects', 'GitHub', 'Resume'],
+    social: ['Facebook', 'LinkedIn', 'Instagram'],
     contact: 'j.olson.digital@gmail.com'
 };
