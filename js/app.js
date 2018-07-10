31042d110b994fd7ca6a62095fca42949b6690d0
Justin Olson <j.olson.digital@gmail.com>
2018-07-09 20:28:41 -0700

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const justin = {
+    location: 'Arizona',
+    interests: ['Coding', 'Gaming', 'Hiking'],
+    languages: ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Bootstrap'],
+    social: ['Facebook', 'LinkedIn', 'Github', 'Instagram'],
+    contact: 'j.olson.digital@gmail.com'
+};
