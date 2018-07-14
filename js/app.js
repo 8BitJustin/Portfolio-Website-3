1f91e19d330222e6fb112460e7cf581c51707890
Justin Olson <j.olson.digital@gmail.com>
2018-07-14 13:06:36 -0700

    Added text and UL to expersite modal. Added some CSS for UL

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const justin = {
     location: 'Arizona',
     interests: ['Coding', 'Gaming', 'Hiking'],
     expertise: () => {
+        console.log('if you see this, whatever you did works.');
         workingSkills();
         basicSkills();
     },
