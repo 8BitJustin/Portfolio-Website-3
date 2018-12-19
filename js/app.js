3ff074e6ea0ac0abc8b152b5662abecd3ac2be72
Justin Olson <j.olson.digital@gmail.com>
2018-12-19 14:49:10 -0700

    Added jquery script to reset coding cert img to shrink to default size when modal is closed. Also removed console testing text.

--- js/app.js
+++ js/app.js
@@ -11,10 +11,13 @@
 // };
 
 const grow = () => {
-    console.log('testing');
     if (document.getElementById('certImg').style.transform === 'scale(1.7)') {
         document.getElementById('certImg').style.transform = 'scale(1)';
     } else {
         document.getElementById('certImg').style.transform = 'scale(1.7)';
     }
 }
+
+$('#coding-modal').on('hidden.bs.modal', function (e) {
+    document.getElementById('certImg').style.transform = 'scale(1)';
+})
