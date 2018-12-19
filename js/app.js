d715a3556593ed1e14069ef312451af87811d57d
Justin Olson <j.olson.digital@gmail.com>
2018-12-19 14:37:19 -0700

    Created grow function within JS. Scales degree img when clicked and reduces when clicked again.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
-const jolsondigital = {
-    name: 'Justin',
-    interests: ['PC Gaming', 'Coding', 'Hiking'],
-    expertise: () => {
-        workingSkills();
-        basicSkills();
-    },
-    work: ['Projects', 'GitHub', 'Resume'],
-    contact: 'j.olson.digital@gmail.com',
-    social: ['Facebook', 'LinkedIn', 'Instagram']
-};
+// const jolsondigital = {
+//     name: 'Justin',
+//     interests: ['PC Gaming', 'Coding', 'Hiking'],
+//     expertise: () => {
+//         workingSkills();
+//         basicSkills();
+//     },
+//     work: ['Projects', 'GitHub', 'Resume'],
+//     contact: 'j.olson.digital@gmail.com',
+//     social: ['Facebook', 'LinkedIn', 'Instagram']
+// };
+
+const grow = () => {
+    console.log('testing');
+    if (document.getElementById('certImg').style.transform === 'scale(1.7)') {
+        document.getElementById('certImg').style.transform = 'scale(1)';
+    } else {
+        document.getElementById('certImg').style.transform = 'scale(1.7)';
+    }
+}
